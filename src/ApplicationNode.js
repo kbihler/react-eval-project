7baea3b3a9114b2089eac8c730d684787114e8d5
Kyle Bihler <kyle@bausermangroup.com>
2018-11-07 17:28:23 -0800

    1

--- src/ApplicationNode.js
+++ src/ApplicationNode.js
@@ -4,6 +4,8 @@ import {Provider} from 'react-redux'
 import {Router, Switch, Route} from 'react-router-dom'
 import history from 'services/history'
 import App from './views/App'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import IconLabelButtons from './views/App/MuiButton'
 
 export default class ApplicationNode extends Component {
   static propTypes = {
@@ -16,15 +18,18 @@ export default class ApplicationNode extends Component {
     const {store} = this.props
 
     return (
-      <div className='application-node'>
-        <Provider store={store}>
-          <Router history={history}>
-            <Switch>
-              <Route exact path='/' component={App} />
-            </Switch>
-          </Router>
-        </Provider>
-      </div>
+      <MuiThemeProvider>
+        <div className='application-node'>
+          <Provider store={store}>
+            <Router history={history}>
+              <Switch>
+                <Route exact path='/' component={App} />
+              </Switch>
+            </Router>
+          </Provider>
+          <IconLabelButtons />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
